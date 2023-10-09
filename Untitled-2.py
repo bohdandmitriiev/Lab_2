f02328070e072a424b16f80ab3842324d62324fe
Богдан Дмитриев <111640301+bohdandmitriiev@users.noreply.github.com>
2023-10-09 14:58:35 +0300

    Update Untitled-2.py

--- Untitled-2.py
+++ Untitled-2.py
@@ -1,4 +1,4 @@
-def get_words_array (sentence):  #параметр функции с которым она работает
+def get_words_array (sentence):  
     words = sentence.split()
     print (f"Кількість символів у реченні-{len(sentence)}")
     print (f"Кількість слів в реченні-{len(words)}")
@@ -8,9 +8,16 @@ def print_letters_count (words_array):
     for word in words_array:
         print (f"Кількість букв у слові \"{word}\"-{len(word)}")
 
-    
+
+def get_reversed_string(string):
+    res=''
+    for i in range(len(string)-1,-1,-1):
+        res+=string[i]
+    print (f"Відзеркалене речення-{res}")
+
 
 sentence = (input("Введіть речення-"))
+get_reversed_string(sentence)
 array = get_words_array(sentence)
 print_letters_count(array)
 
